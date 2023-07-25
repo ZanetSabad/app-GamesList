e1742401f3684abc314d2552d88f5ecbb5006de8
Žaneta Sabadka <zaneta.sabadkova@gmail.com>
2023-07-25 07:59:33 +0200

    přidána data.js a složky images+obrázky a components

---
+++ src/data.js
@@ -0,0 +1,95 @@
+import image1 from "./images/game1.jpg"
+import image2 from "./images/game2.jpg"
+import image3 from "./images/game3.jpg"
+import image4 from "./images/game4.jpg"
+import image5 from "./images/game5.jpg"
+import image6 from "./images/game6.jpg"
+import image7 from "./images/game7.jpg"
+import image8 from "./images/game8.jpg"
+import image9 from "./images/game9.jpg"
+import image10 from "./images/game10.jpg"
+
+const allGames = [
+    {
+        id: 1,
+        image: image2,
+        title: "Witcher 2: Assassins of Kings",
+        age: "+18",
+        tags: "Fantasy * RPG",
+        description: "Jedno z nejlepších příběhových RPG, které se objevily na konzolích. Rychle koupit a hrát.Šup, šup.",
+    },
+    {
+        id: 2,
+        image: image1,
+        title: "Witcher 3: Wild Hunt",
+        age: "+18",
+        tags: "Fantasy * RPG",
+        description: "Živý, vizuálně propracovaný do nejjemnějších detailů. Rozsáhlý příběh, ve kterém bude osud světa stát na vašem úsudku a svědomí.",
+    },
+    {
+        id: 3,
+        image: image3,
+        title: "Assassins´s creed Odyssey",
+        age: "+18",
+        tags: "Akční * RPG * Historické",
+        description: "Napište vlastní odyseu a prožijte neuvěřitelná dobrodružství ve světě, kde i z vyhnance se může stát řeský hrdina.",
+    },
+    {
+        id: 4,
+        image: image4,
+        title: "Assassin's Creed: Mirage",
+        age: "+18",
+        tags: "Akční * RPG * Historické",
+        description: "Objevte příběh o Basimovi, chytrém pouličním zlodějíčkovi s děsivými vidinami, který hledá odpovědi v rušných ulicích Bagdádu. ",
+    },
+    {
+        id: 5,
+        image: image5,
+        title: "Overcooked 2 Arrives In August On Switch",
+        age: "+3",
+        tags: "Rodina * Děti ",
+        description: "Připravte se na pořádnou porci vaření a neuvěřitelného chaosu",
+    },
+    {
+        id: 6,
+        image: image6,
+        title: "Counter-Strike: GO",
+        age: "+18",
+        tags: "Násilí * Napínavý",
+        description: "Zahrajte si taktickou střílečku, která Vás vtáhne do nebezpečí. Na co ještě čekáte? Buďte se svým přáteli online a hrajte GO GO GO!,"
+    },
+    {
+        id: 7,
+        image: image7,
+        title: "It Takes Two",
+        age: "+12",
+        tags: "Arkáda * Dobrodružná",
+        description: "Jste rozhádaná manželská dvojice, která se kouzlem promění v panenky. Dostanete se do fantastického světa, v němž vás čeká spousta nepředvídatelných situací. Pokud je chcete zvládnout, musíte spolupracovat.",
+    },
+    {
+        id: 8,
+        image: image8,
+        title: "Hogwarts Legacy",
+        age: "+12",
+        tags: "Akční * RPG",
+        description: "Vydejte se do světa kouzel a bojových dovedností a hledání přátel. Na nebezpečné cestě budete čelit smrtícím nepřátelům a možná odkryjete skrytou pravdu.",
+    },
+    {
+        id: 9,
+        image: image9,
+        title: "Avatar: Frontiers of Pandora",
+        age: "+16",
+        tags: "Akční * Dobrodružný",
+        description: "Vás, Na'vi, unesenla lidská korporace a chtějí aby jste sloužili jejich účelům. Vy se po letech vysvobodíte, ale ocitnete se jako cizinec ve svém rodišti. Spojte se znovu se svým ztraceným dědictvím a zjistěte, co skutečně znamená být Na'vi.",
+    },
+    {
+        id: 10,
+        image: image10,
+        title: "Ori and the Will of the Wisps",
+        age: "+7",
+        tags: "Akční * Napínavý ",
+        description: "Stanete se bílým guirdian spiritem jménem Ori. Pomáháte mu vyřešit různé hlavolamy a hádanky a vylepšování Oriho schopnosti.",
+    },
+]
+
+export default allGames
